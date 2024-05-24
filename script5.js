f50c17b536869a28969aeff560eec4da729db252
Aparna-kumari-nayak <166199867+Aparna-kumari-nayak@users.noreply.github.com>
2024-05-24 10:27:42 +0530

    Add files via upload

---
+++ script5.js
@@ -0,0 +1,40 @@
+var url = 'https://randomuser.me/api/';
+var btn = document.querySelector("#btn");
+var avatar = document.querySelector("#avatar");
+var fullname = document.querySelector("#fullname");
+var username = document.querySelector("#username");
+var email = document.querySelector("#email");
+var city = document.querySelector("#city");
+
+btn.addEventListener("click", function(){
+  fetch(url)
+  .then(handleErrors)
+  .then(parseJSON)
+  .then(updateProfile)
+  .catch(printError)
+});
+        
+function handleErrors(res){
+  if(!res.ok){
+    throw Error(res.status);
+  }
+  return res;
+}
+
+function parseJSON(res){
+  return res.json().then(function(parsedData){
+    return parsedData.results[0];
+  })
+}
+
+function updateProfile(data){
+  avatar.src = data.picture.medium;
+  fullname.innerText = data.name.first + " " + data.name.last;
+  username.innerText = data.login.username
+  email.innerText = data.email;
+  city.innerText = data.location.city;
+}
+
+function printError(error){
+  console.log(error);
+}
